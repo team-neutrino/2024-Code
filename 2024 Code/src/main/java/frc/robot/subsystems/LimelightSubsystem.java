1500d84ce9411f92ad8c124e22f7ec3cd82a94b8
bhebhrus <88865109+bhebhrus@users.noreply.github.com>
2024-01-09 17:56:15 -0600

    start

---
+++ 2024 Code/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class LimelightSubsystem {
+    private NetworkTable limelight;
+    public LimelightSubsystem() {
+    // global instance of the network table and gets the limelight table
+    limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
+    // turns off LED
+    limelight.getEntry("ledMode").setNumber(1);
+  }
+  public boolean getTv() {
+    NetworkTableEntry tv = limelight.getEntry("tv");
+    double validTarget = tv.getDouble(0.0);
+    if (validTarget == 1) {
+      return true;
+    }
+    return false;
+  }
+  public double getID() {
+    return limelight.getEntry("tid").getDouble(0.0);
+  }
+
+  // gets the x offest between the center of vision and the detected object
+  public double getTx() {
+    return limelight.getEntry("tx").getDouble(0.0);
+  }
+
+  // gets the y offest between the center of vision and the detected object
+  public double getTy() {
+    return limelight.getEntry("ty").getDouble(0.0);
+  }
+  public void periodic() {
+    limelight.getEntry("ledMode").setNumber(1);  }
+}
