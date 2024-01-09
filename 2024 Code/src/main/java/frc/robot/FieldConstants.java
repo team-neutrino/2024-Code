0dcd50a230c8c7c0a869ed49d35a228f74e6807b
Noah9847 <70918360+CaleWineinger@users.noreply.github.com>
2024-01-09 15:45:42 -0800

    add field constants

--- 2024 Code/src/main/java/frc/robot/FieldConstants.java
+++ 2024 Code/src/main/java/frc/robot/FieldConstants.java
@@ -4,6 +4,8 @@ import edu.wpi.first.math.*;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
 
 import java.util.List;
 import java.util.Map;
@@ -12,24 +14,24 @@ import java.util.Map;
 public final class FieldConstants{
     public static final Map<Integer, Pose3d> tenAprilTags =
     Map.of(
-        1, new Pose3d(593.68, 9.68, 53.38, new Rotation3d(0, 0, (2*Math.PI)/3)),
-        2, new Pose3d(637.21, 34.79, 53.38, new Rotation3d(0, 0, (2*Math.PI)/3)),
-        3, new Pose3d(652.73, 196.17, 57.13, new Rotation3d(0, 0, Math.PI)),
-        4, new Pose3d(652.73, 218.42, 57.13, new Rotation3d(0, 0, Math.PI)),
-        5, new Pose3d(578.77, 323, 53.38, new Rotation3d(0, 0, (3*Math.PI)/2)),
-        6, new Pose3d(72.5, 323, 53.38, new Rotation3d(0, 0, (3*Math.PI)/2)),
-        7, new Pose3d(-1.5, 218.42, 57.13, new Rotation3d()),
-        8, new Pose3d(-1.5, 196.17, 57.13, new Rotation3d()),
-        9, new Pose3d(14.02, 34.79, 53.38, new Rotation3d(0, 0, Math.PI/3)),
-        10, new Pose3d(57.54, 9.68, 53.58, new Rotation3d(0, 0, Math.PI/3))
+        1, new Pose3d(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (2*Math.PI)/3)),
+        2, new Pose3d(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (2*Math.PI)/3)),
+        3, new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13), new Rotation3d(0, 0, Math.PI)),
+        4, new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13), new Rotation3d(0, 0, Math.PI)),
+        5, new Pose3d(Units.inchesToMeters(578.77), Units.inchesToMeters(323), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (3*Math.PI)/2)),
+        6, new Pose3d(Units.inchesToMeters(72.5), Units.inchesToMeters(323), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (3*Math.PI)/2)),
+        7, new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13), new Rotation3d()),
+        8, new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13), new Rotation3d()),
+        9, new Pose3d(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38), new Rotation3d(0, 0, Math.PI/3)),
+        10, new Pose3d(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68), Units.inchesToMeters(53.58), new Rotation3d(0, 0, Math.PI/3))
     );
     public static final Map<Integer, Pose3d> sixteenAprilTags = 
     Map.of(
-        11, new Pose3d(468.69, 146.19, 52, new Rotation3d(0, 0, (5*Math.PI)/3)),
-        12, new Pose3d( 468.69, 177.10, 52, new Rotation3d(0, 0, Math.PI/3)),
-        13, new Pose3d(441.73, 161.62, 52, new Rotation3d(0, 0, Math.PI)),
-        14, new Pose3d(209.48, 161.62, 52, new Rotation3d()),
-        15, new Pose3d(182.73, 177.10, 52, new Rotation3d(0, 0, (2*Math.PI)/3)),
-        16, new Pose3d(182.73, 146.19, 52, new Rotation3d(0, 0, (4*Math.PI)/3))
+        11, new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19), Units.inchesToMeters(52), new Rotation3d(0, 0, (5*Math.PI)/3)),
+        12, new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10), Units.inchesToMeters(52), new Rotation3d(0, 0, Math.PI/3)),
+        13, new Pose3d(Units.inchesToMeters(441.73), Units.inchesToMeters(161.62), Units.inchesToMeters(52), new Rotation3d(0, 0, Math.PI)),
+        14, new Pose3d(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62), Units.inchesToMeters(52), new Rotation3d()),
+        15, new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10), Units.inchesToMeters(52), new Rotation3d(0, 0, (2*Math.PI)/3)),
+        16, new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19), Units.inchesToMeters(52), new Rotation3d(0, 0, (4*Math.PI)/3))
     );
 }
