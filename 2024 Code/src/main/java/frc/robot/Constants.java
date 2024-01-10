0906028442415dc204744d5613e77ed0a12519ed
Adam Zhu <887022zhu@ames.k12.ia.us>
2024-01-10 17:01:22 -0600

    added subclass to constants
    w/ anirudh

--- 2024 Code/src/main/java/frc/robot/Constants.java
+++ 2024 Code/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class MotorConstants {
+    public static int shooter;
+  }
 }
