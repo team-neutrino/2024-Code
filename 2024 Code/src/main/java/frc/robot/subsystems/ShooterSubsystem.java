4b5361d3477c127580e907ee0f36da607efacd88
Neutrino Controls <123705736+ViennaRossmanith@users.noreply.github.com>
2024-01-10 17:27:59 -0600

    fro adam

--- 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,27 +6,39 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.MotorConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
    private CANSparkMax m_shooter =
       new CANSparkMax(MotorConstants.shooter, MotorType.kBrushless);
       private RelativeEncoder m_shooterEncoder;
+      private JoystickButton m_bumperRight;
+    
 
-  public ShooterSubsystem(){
+  public ShooterSubsystem(JoystickButton p_bumperRight){
+    m_bumperRight = p_bumperRight;
     m_shooter.restoreFactoryDefaults();
     m_shooter.setIdleMode(IdleMode.kBrake);
   }
-  public double shooterEncoder() {
+  public double getshooterEncoder() {
     return m_shooterEncoder.getPosition();
   }
 
-  public double getR1Vel() {
+  public double getshooterVel() {
     return m_shooterEncoder.getVelocity();
   }
   public void resetEncoders(RelativeEncoder encoder){
     encoder.setPosition(0);
   }
+  public void setVoltage(double voltage) {
+    m_shooter.setVoltage(voltage);
+  }
+
+  public void getVoltage(double voltage){
+    m_shooter.setVoltage(voltage);
+  }
+
   public void periodic() {
     
    }
