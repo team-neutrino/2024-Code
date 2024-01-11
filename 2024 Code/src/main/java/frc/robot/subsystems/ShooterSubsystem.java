8f55b2db33b697a349e0e2961b1d40edc5f7d807
Neutrino Controls <123705736+ViennaRossmanith@users.noreply.github.com>
2024-01-10 18:48:45 -0600

    pid setup

--- 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -14,12 +15,26 @@ public class ShooterSubsystem extends SubsystemBase {
       new CANSparkMax(MotorConstants.shooter, MotorType.kBrushless);
       private RelativeEncoder m_shooterEncoder;
       private JoystickButton m_bumperRight;
+      private SparkMaxPIDController m_pidController;
+      private  double WHEEL_P;
+      private  double WHEEL_I;
+      private  double WHEEL_D;
+      private  double WHEEL_FF;
     
 
   public ShooterSubsystem(JoystickButton p_bumperRight){
     m_bumperRight = p_bumperRight;
+    m_pidController = m_shooter.getPIDController();
+    m_pidController.setFeedbackDevice(m_shooterEncoder);
     m_shooter.restoreFactoryDefaults();
     m_shooter.setIdleMode(IdleMode.kBrake);
+
+    m_pidController.setP(WHEEL_P / 1000.0);
+    m_pidController.setI(WHEEL_I / 1000.0);
+    m_pidController.setD(WHEEL_D / 1000.0);
+    m_pidController.setFF(WHEEL_FF / 1000.0);
+    m_pidController.setIZone(160);
+    m_pidController.setOutputRange(.1, 1);
   }
   public double getshooterEncoder() {
     return m_shooterEncoder.getPosition();
@@ -38,6 +53,38 @@ public class ShooterSubsystem extends SubsystemBase {
   public void getVoltage(double voltage){
     m_shooter.setVoltage(voltage);
   }
+  public double getP() {
+    return m_pidController.getP() * 1000.0;
+  }
+
+  public double getFF() {
+    return m_pidController.getFF() * 1000.0;
+  }
+
+  public void setP(double P) {
+    m_pidController.setP(P / 1000.0);
+  }
+
+  public double getI() {
+    return m_pidController.getI() * 1000.0;
+  }
+
+  public void setI(double I) {
+    m_pidController.setI(I / 1000.0);
+  }
+
+  public double getD() {
+    return m_pidController.getD() * 1000.0;
+  }
+
+  public void setD(double D) {
+    m_pidController.setD(D / 1000.0);
+  }
+
+  public void setFF(double FF) {
+    m_pidController.setFF(FF / 1000.0);
+  }
+
 
   public void periodic() {
     
