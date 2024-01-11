75d6f00922d385d34a5c497c6ce93d9ce1f4d4dd
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2024-01-10 19:38:52 -0600

    hehe

---
+++ 2024 Code/src/main/java/frc/robot/util/SubsystemContainer.java
@@ -0,0 +1,10 @@
+package frc.robot.util;
+
+import frc.robot.subsystems.LimelightSubsystem;
+
+public class SubsystemContainer {
+    
+    public static final LimelightSubsystem limelightSubsystem = new LimelightSubsystem();
+
+
+}
