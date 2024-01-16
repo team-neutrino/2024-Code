56c91a9757e2f1001f759ba4919310b137876bb4
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2024-01-16 17:47:03 -0600

    commit

--- 2024 Code/src/main/java/frc/robot/FieldConstants.java
+++ 2024 Code/src/main/java/frc/robot/FieldConstants.java
@@ -11,38 +11,55 @@ import java.util.List;
 import java.util.Map;
 
 // All values for the pose3d are in inches and the values for the rotation3d are in radians!
-public final class FieldConstants{
-    public static final Map<Integer, Pose3d> tenAprilTags =
-    Map.of(
-        1, new Pose3d(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (2*Math.PI)/3)),
-        2, new Pose3d(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (2*Math.PI)/3)),
-        3, new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13), new Rotation3d(0, 0, Math.PI)),
-        4, new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13), new Rotation3d(0, 0, Math.PI)),
-        5, new Pose3d(Units.inchesToMeters(578.77), Units.inchesToMeters(323), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (3*Math.PI)/2)),
-        6, new Pose3d(Units.inchesToMeters(72.5), Units.inchesToMeters(323), Units.inchesToMeters(53.38), new Rotation3d(0, 0, (3*Math.PI)/2)),
-        7, new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13), new Rotation3d()),
-        8, new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13), new Rotation3d()),
-        9, new Pose3d(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38), new Rotation3d(0, 0, Math.PI/3)),
-        10, new Pose3d(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68), Units.inchesToMeters(53.58), new Rotation3d(0, 0, Math.PI/3))
-    );
-    public static final Map<Integer, Pose3d> sixteenAprilTags = 
-    Map.of(
-        11, new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19), Units.inchesToMeters(52), new Rotation3d(0, 0, (5*Math.PI)/3)),
-        12, new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10), Units.inchesToMeters(52), new Rotation3d(0, 0, Math.PI/3)),
-        13, new Pose3d(Units.inchesToMeters(441.73), Units.inchesToMeters(161.62), Units.inchesToMeters(52), new Rotation3d(0, 0, Math.PI)),
-        14, new Pose3d(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62), Units.inchesToMeters(52), new Rotation3d()),
-        15, new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10), Units.inchesToMeters(52), new Rotation3d(0, 0, (2*Math.PI)/3)),
-        16, new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19), Units.inchesToMeters(52), new Rotation3d(0, 0, (4*Math.PI)/3))
-    );
-    //FORMAT: M, C, X1, X2 (X, Z1, Z2 if null)
-    public static final Double[][] bannedRegions = {
-        {null, 2.423414, -1.557274, 1.557274},
-        {null, -2.423414, -1.557274, 1.557274},
-        {-0.55635208711, 2.90554543682, 2.423414, 5.222494},
-        {0.55635208711, 2.90554543682, -5.222494, 2.423414},
-        {0.55635208711, -2.90554543682, 2.423414, 5.222494},
-        {-0.55635208711, -2.90554543682, -5.222494, 2.423414}
-    };
+public final class FieldConstants {
+    public static final Map<Integer, Pose3d> tenAprilTags = Map.of(
+            1,
+            new Pose3d(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38),
+                    new Rotation3d(0, 0, (2 * Math.PI) / 3)),
+            2,
+            new Pose3d(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38),
+                    new Rotation3d(0, 0, (2 * Math.PI) / 3)),
+            3,
+            new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13),
+                    new Rotation3d(0, 0, Math.PI)),
+            4,
+            new Pose3d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13),
+                    new Rotation3d(0, 0, Math.PI)),
+            5,
+            new Pose3d(Units.inchesToMeters(578.77), Units.inchesToMeters(323), Units.inchesToMeters(53.38),
+                    new Rotation3d(0, 0, (3 * Math.PI) / 2)),
+            6,
+            new Pose3d(Units.inchesToMeters(72.5), Units.inchesToMeters(323), Units.inchesToMeters(53.38),
+                    new Rotation3d(0, 0, (3 * Math.PI) / 2)),
+            7,
+            new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13),
+                    new Rotation3d()),
+            8,
+            new Pose3d(Units.inchesToMeters(-1.5), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13),
+                    new Rotation3d()),
+            9,
+            new Pose3d(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38),
+                    new Rotation3d(0, 0, Math.PI / 3)),
+            10, new Pose3d(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68), Units.inchesToMeters(53.58),
+                    new Rotation3d(0, 0, Math.PI / 3)));
+    public static final Map<Integer, Pose3d> sixteenAprilTags = Map.of(
+            11,
+            new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19), Units.inchesToMeters(52),
+                    new Rotation3d(0, 0, (5 * Math.PI) / 3)),
+            12,
+            new Pose3d(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10), Units.inchesToMeters(52),
+                    new Rotation3d(0, 0, Math.PI / 3)),
+            13,
+            new Pose3d(Units.inchesToMeters(441.73), Units.inchesToMeters(161.62), Units.inchesToMeters(52),
+                    new Rotation3d(0, 0, Math.PI)),
+            14,
+            new Pose3d(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62), Units.inchesToMeters(52),
+                    new Rotation3d()),
+            15,
+            new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10), Units.inchesToMeters(52),
+                    new Rotation3d(0, 0, (2 * Math.PI) / 3)),
+            16, new Pose3d(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19), Units.inchesToMeters(52),
+                    new Rotation3d(0, 0, (4 * Math.PI) / 3)));
 
-    public static final Double[] autoAlignPoint = {1.0, 1.0, 1.0};
+    public static final Double[] autoAlignPoint = { 1.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
 }
--- 2024 Code/src/main/java/frc/robot/commands/AutoAlign.java
+++ 2024 Code/src/main/java/frc/robot/commands/AutoAlign.java
@@ -10,47 +10,37 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
 public class AutoAlign extends Command {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-  private final LimelightSubsystem m_limelight;
-  boolean contact = false;
+    @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+    private final LimelightSubsystem m_limelight;
+    double[] pose;
+    double yawChange;
 
-  public AutoAlign(LimelightSubsystem limelight) {
-    m_limelight = limelight;
-    addRequirements(limelight);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    double[] pose = m_limelight.getBotPose();
-    double mNaught = (FieldConstants.autoAlignPoint[2] - pose[2]) / (FieldConstants.autoAlignPoint[0] - pose[0]);
-    double cNaught = pose[2] - mNaught;
-    for (Double[] region : FieldConstants.bannedRegions) {
-      if (region[0] != null) {
-        double x_i = (region[1] - cNaught) / (region[0] - mNaught);
-        if (x_i > region[3] && x_i < region[4]) {
-          contact = true;
-        }
-      } else {
-        double z_i = mNaught * region[1] + cNaught;
-
-      }
+    public AutoAlign(LimelightSubsystem limelight) {
+        m_limelight = limelight;
+        addRequirements(limelight);
     }
-  }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        // in degrees?
+        setRobotYaw(FieldConstants.autoAlignPoint[5]);
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
