01826240bab0de3208c1e61d6a1c4139a2b940a3
Noah9847 <70918360+CaleWineinger@users.noreply.github.com>
2024-01-17 18:06:26 -0600

    yep

--- 2024 Code/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Code/src/main/java/frc/robot/RobotContainer.java
@@ -5,70 +5,50 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ClimbDefaultCommand;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ClimbSubsystem;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.commands.LEDCommand;
+import frc.robot.commands.LEDDefaultCommand;
+import frc.robot.commands.SwerveDefaultCommand;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.commands.IntakeDefaultCommand;
+import frc.robot.commands.IntakeReverseCommand;
+import frc.robot.util.SubsystemContainer;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  //subsystems
-  private final LimelightSubsystem m_LimelightSubsystem = new LimelightSubsystem();
-  private final ClimbSubsystem m_ClimbSubsystem = new ClimbSubsystem();
 
-  //default commands
-  private final ClimbDefaultCommand m_climbDefaultCommand = new ClimbDefaultCommand();
+  CommandXboxController m_controller = new CommandXboxController(OperatorConstants.XBOX_CONTROLLER);
+  CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(
-      OperatorConstants.kDriverControllerPort);
+  SwerveDefaultCommand m_swerveDefaultCommand = new SwerveDefaultCommand(m_controller);
+  LEDDefaultCommand m_LEDDefaultCommand = new LEDDefaultCommand();
+  IntakeDefaultCommand m_IntakeDefaultCommand = new IntakeDefaultCommand();
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
   public RobotContainer() {
-    // Configure the trigger bindings
+
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be
-   * created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-   * an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link
-   * CommandXboxController
-   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or
-   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
     //set default commands
-    m_ClimbSubsystem.setDefaultCommand(m_climbDefaultCommand);
+
+    m_controller.a().whileTrue(new LEDCommand());
+
+    m_controller.b().whileTrue(new IntakeCommand());
+
+    m_controller.y().whileTrue(new IntakeReverseCommand());
+
+    SubsystemContainer.LEDSubsystem.setDefaultCommand(m_LEDDefaultCommand);
+
+    SubsystemContainer.swerveSubsystem.setDefaultCommand(m_swerveDefaultCommand);
+
+    SubsystemContainer.intakeSubsystem.setDefaultCommand(m_IntakeDefaultCommand);
+
+    SubsystemContainer.climbSubsystem.setDefaultCommand(m_IntakeDefaultCommand);
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    return null;
+    return Commands.print("No autonomous command configured");
   }
 }
--- 2024 Code/src/main/java/frc/robot/util/SubsystemContainer.java
+++ 2024 Code/src/main/java/frc/robot/util/SubsystemContainer.java
@@ -1,9 +1,17 @@
 package frc.robot.util;
 
+import frc.robot.subsystems.ClimbSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class SubsystemContainer {
 
     public static final LimelightSubsystem limelightSubsystem = new LimelightSubsystem();
+    public static final LEDSubsystem LEDSubsystem = new LEDSubsystem();
+    public static final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+    public static final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    public static final ClimbSubsystem climbSubsystem = new ClimbSubsystem();
 
 }
