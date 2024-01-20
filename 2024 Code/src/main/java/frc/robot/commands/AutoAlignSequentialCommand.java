9c02221f4ce0c26ec925840e1796e60f6176aa37
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2024-01-20 11:47:11 -0600

    FCKF YFOEW*HJPOIUFEWJFEW

---
+++ 2024 Code/src/main/java/frc/robot/commands/AutoAlignSequentialCommand.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.FieldConstants;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.util.SubsystemContainer;
+import edu.wpi.first.hal.AllianceStationID;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class AutoAlignSequentialCommand extends Command {
+    double currentYaw;
+    double targetYaw;
+    private LimelightSubsystem m_limelight;
+    private SwerveSubsystem m_swerveSubsystem;
+
+    public AutoAlignSequentialCommand() {
+        m_limelight = SubsystemContainer.limelightSubsystem;
+        m_swerveSubsystem = SubsystemContainer.swerveSubsystem;
+        addRequirements(m_limelight, m_swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        // in degrees?
+        currentYaw = m_limelight.getBotPose()[5];
+        targetYaw = m_limelight.getTargetPose()[5];
+        m_swerveSubsystem.setRobotYaw(currentYaw - targetYaw);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        if()
+        return false;
+    }
+}
