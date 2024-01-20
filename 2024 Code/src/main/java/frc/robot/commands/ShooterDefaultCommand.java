2b4bca97e93b9f8a422580b988644f33faf942fe
Adam Zhu <117839284+ssarahyao@users.noreply.github.com>
2024-01-19 19:49:30 -0600

    sdfsdfsdf

--- 2024 Code/src/main/java/frc/robot/commands/ShooterDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/ShooterDefaultCommand.java
@@ -4,24 +4,17 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.util.SubsystemContainer;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/** An example command that uses an example subsystem. */
 public class ShooterDefaultCommand extends Command {
     @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
-     */
     public ShooterDefaultCommand() {
         addRequirements(SubsystemContainer.ShooterSubsystem);
     }
 
-    // Called when the command is initially scheduled.
+
     public void initialize() {
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         SubsystemContainer.ShooterSubsystem.setTargetRPM(1000);
