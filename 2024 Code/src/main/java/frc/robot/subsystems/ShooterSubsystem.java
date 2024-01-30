02af0ed600692a0193b11be928801b2569967c4e
Anirudh <115200816+Anirudh4Neutrino@users.noreply.github.com>
2024-01-29 18:06:16 -0600

    ... I hope this worked

--- 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -29,6 +29,9 @@ public class ShooterSubsystem extends SubsystemBase {
   final private double APPROVE_COUNTER_THRESHOLD = 9;
   final private double COUNTER_ERROR_THRESHOLD = 10;
 
+  //this is a change and a test
+
+  public final PIDChangerSimulation PIDSimulation = new PIDChangerSimulationShooter(WHEEL_P,WHEEL_I, WHEEL_D, WHEEL_FF);
   public ShooterSubsystem() {
     m_shooterEncoder = m_shooter.getEncoder();
     m_pidController = m_shooter.getPIDController();
