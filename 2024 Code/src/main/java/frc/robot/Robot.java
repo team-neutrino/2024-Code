d6ad26ae9c960e71b4467c45d5c5a00e5b275a99
Noah9847 <williamsonoah0@gmail.com>
2024-02-05 19:22:09 -0600

    who???

--- 2024 Code/src/main/java/frc/robot/Robot.java
+++ 2024 Code/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
