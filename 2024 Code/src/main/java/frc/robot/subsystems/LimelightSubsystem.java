b64b2f8bb189fac752f44feb261b604a0e4db0ef
Allen <152452752+allencai11@users.noreply.github.com>
2024-02-10 11:30:05 -0600

    eer 2

--- 2024 Code/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -5,12 +5,16 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.subsystems.simulation.LimelightSimulation;
+
 public class LimelightSubsystem extends SubsystemBase {
   private NetworkTable limelight;
   private double[] pose = new double[6];
   private double[] targetPose = new double[6];
   private double[] pastPose = new double[6];
   private double[] pastTargetPose = new double[6];
+
+  public final LimelightSimulation m_LimelightSimulation = new LimelightSimulation();
 
   public LimelightSubsystem() {
     // global instance of the network table and gets the limelight table
@@ -21,24 +25,24 @@ public class LimelightSubsystem extends SubsystemBase {
 
   public boolean getTv() {
     NetworkTableEntry tv = limelight.getEntry("tv");
-    double validTarget = tv.getDouble(0.0);
+    double validTarget = tv.getDouble(m_LimelightSimulation.GetTy());
 
     return validTarget == 1;
   }
 
   public Double getID() {
-    Double id = limelight.getEntry("tid").getDouble(0.0);
+    Double id = limelight.getEntry("tid").getDouble(m_LimelightSimulation.GetId());
     return id;
   }
 
   // gets the x offest between the center of vision and the detected object
   public double getTx() {
-    return limelight.getEntry("tx").getDouble(0.0);
+    return limelight.getEntry("tx").getDouble(m_LimelightSimulation.GetTx());
   }
 
   // gets the y offest between the center of vision and the detected object
   public double getTy() {
-    return limelight.getEntry("ty").getDouble(0.0);
+    return limelight.getEntry("ty").getDouble(m_LimelightSimulation.GetTy());
   }
 
   public void periodic() {
