8a0002709a1c66bf7ddbc0bbcd6771e6d79ed882
Allen Cai <sarahames2008@gmail.com>
2024-02-15 19:26:38 -0600

    photonvision

---
+++ 2024 Code/src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -0,0 +1,67 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSubsystem extends SubsystemBase {
+  private NetworkTable limelight;
+  private double[] pose = new double[6];
+  private double[] targetPose = new double[6];
+  private double[] pastPose = new double[6];
+  private double[] pastTargetPose = new double[6];
+
+  public LimelightSubsystem() {
+    // global instance of the network table and gets the limelight table
+    limelight = NetworkTableInstance.getDefault().getTable("limelight");
+    // turns off LED
+    limelight.getEntry("ledMode").setNumber(1);
+  }
+
+  public boolean getTv() {
+    NetworkTableEntry tv = limelight.getEntry("tv");
+    double validTarget = tv.getDouble(0.0);
+
+    return validTarget == 1;
+  }
+
+  public Double getID() {
+    Double id = limelight.getEntry("tid").getDouble(0.0);
+    return id;
+  }
+
+  // gets the x offest between the center of vision and the detected object
+  public double getTx() {
+    return limelight.getEntry("tx").getDouble(0.0);
+  }
+
+  // gets the y offest between the center of vision and the detected object
+  public double getTy() {
+    return limelight.getEntry("ty").getDouble(0.0);
+  }
+
+  public void periodic() {
+    limelight.getEntry("ledMode").setNumber(1);
+  }
+
+  public double[] getBotPose() {
+    pose = limelight.getEntry("botpose").getDoubleArray(pastPose);
+    if (getTv()) {
+      pastPose = pose;
+    }
+    return pose;
+  }
+
+  public double[] getTargetPose() {
+    targetPose = limelight.getEntry("targetpose_robotspace").getDoubleArray(pastTargetPose);
+    if (getTv()) {
+      pastTargetPose = targetPose;
+    }
+    return targetPose;
+  }
+
+  public void setPipeline(int pipeline) {
+    limelight.getEntry("pipeline").setNumber(pipeline);
+  }
+}
