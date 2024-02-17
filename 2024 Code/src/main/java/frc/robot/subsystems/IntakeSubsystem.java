76c51ea2d4c7d50c43da60987d945eb935ff3e2c
Florida Cracker <sarahames2008@gmail.com>
2024-02-16 19:25:08 -0600

    New with the changes

--- 2024 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -45,7 +45,7 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void runIntake() {
-        m_intakeMotor.setVoltage(-IntakeConstants.INTAKE_MOTOR_VOLTAGE);
+        m_intakeMotor.setVoltage(IntakeConstants.INTAKE_MOTOR_VOLTAGE);
     }
 
     public void runIndex() {
