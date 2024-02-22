89f2c7f20298d64c089f189da2017ec756c330dd
Vienna Rossmanith <887432ros@ames.k12.ia.us>
2024-02-22 17:33:58 -0600

    c

--- 2024 Code/src/main/java/frc/robot/SwerveModule.java
+++ 2024 Code/src/main/java/frc/robot/SwerveModule.java
@@ -70,9 +70,6 @@ public class SwerveModule {
                 Constants.DimensionConstants.WHEEL_CIRCUMFERENCE / Constants.SwerveConstants.GEAR_RATIO);
         speedEncoder.setVelocityConversionFactor(
                 Constants.DimensionConstants.WHEEL_CIRCUMFERENCE / (60 * Constants.SwerveConstants.GEAR_RATIO));
-
-        speedEncoder.setInverted(true);
-
         anglePID = angleMotor.getPIDController();
         speedPID = speedMotor.getPIDController();
         anglePID.setFeedbackDevice(absAngleEncoder);
@@ -119,7 +116,7 @@ public class SwerveModule {
             angle += OFFSET - 360;
         }
 
-        return (angle + 180) % 360;
+        return angle;
     }
 
     public void setAnglePID(double reference) {
