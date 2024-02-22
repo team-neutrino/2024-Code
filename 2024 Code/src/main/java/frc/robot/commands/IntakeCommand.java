d4064162b24c35006313a90198679a3e63e98729
Allen Cai <allencaicm@gmail.com>
2024-02-21 19:02:35 -0600

    intake stuff

--- 2024 Code/src/main/java/frc/robot/commands/IntakeCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.util.SubsystemContainer;
 
@@ -22,9 +23,13 @@ public class IntakeCommand extends Command {
 
   }
 
+  // Math.abs(SubsystemContainer.armSubsystem.getTargetAngle() -
+  // Constants.ArmConstants.ARM_LOWER_LIMIT) < 2
   @Override
   public void execute() {
-    if (m_intakeSubsystem.getBeamBreak()) {
+    if (m_intakeSubsystem.getBeamBreak()
+        && SubsystemContainer.armSubsystem.getTargetAngle() == Constants.ArmConstants.ARM_LOWER_LIMIT
+        && SubsystemContainer.armSubsystem.getInPosition()) {
       m_intakeSubsystem.runIntake();
       m_intakeSubsystem.runIndexIntake();
     } else {
