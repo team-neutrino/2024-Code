17a7247b47bbc50d6739102f43a1215e82b71da2
Noah9847 <williamsonoah0@gmail.com>
2024-03-02 14:53:31 -0600

    c

--- 2024 Code/src/main/java/frc/robot/commands/MagicSpeakerCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/MagicSpeakerCommand.java
@@ -52,7 +52,7 @@ public class MagicSpeakerCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (m_intakeSubsystem.getBeamBreak()) {
+    if (!m_intakeSubsystem.isBeamBroken()) {
       i++;
       if (i > 10) {
         return true;
