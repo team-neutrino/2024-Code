1eb14bf1f0e2d374c4f7b8db8fe7a3ea9d5ca0d0
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-03-04 17:14:31 -0600

    a

--- 2024 Code/src/main/java/frc/robot/commands/FinishJitterCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/FinishJitterCommand.java
@@ -9,41 +9,39 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.util.SubsystemContainer;
 
-public class IndexJitterCommand extends Command {
-  private IntakeSubsystem m_intake;
-  private Timer m_timer;
+public class FinishJitterCommand extends Command {
 
-  public IndexJitterCommand() {
+  private Timer timer;
+  private IntakeSubsystem m_intake;
+
+  /** Creates a new FinishJitterCommand. */
+  public FinishJitterCommand() {
+    timer = new Timer();
     m_intake = SubsystemContainer.intakeSubsystem;
-    m_timer = new Timer();
     addRequirements(m_intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_timer.start();
+    timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intake.indexJitter();
+    System.out.println("finish jitter running");
+    m_intake.runIndexIntake();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_timer.stop();
-    m_timer.reset();
-    if (!m_intake.isBeamBroken() && interrupted) {
-      new FinishJitterCommand().schedule();
-    }
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (m_timer.get() > 2 && m_intake.isBeamBroken());
+    return false;
   }
 }
