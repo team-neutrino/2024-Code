9b149293e23ddfa677ceac73ec7f2f2ae3fba342
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-03-04 16:51:21 -0600

    Update MagicSpeakerCommand.java

--- 2024 Code/src/main/java/frc/robot/commands/MagicSpeakerCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/MagicSpeakerCommand.java
@@ -4,11 +4,7 @@
 
 package frc.robot.commands;
 
-import javax.management.InstanceAlreadyExistsException;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.util.SubsystemContainer;
 import frc.robot.util.CalculateAngle;
 import frc.robot.subsystems.ArmSubsystem;
