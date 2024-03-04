c69e3289a15bb80f4be59a8254c721ecdce0489b
Noah9847 <williamsonoah0@gmail.com>
2024-03-04 17:04:14 -0600

    arm things?

--- 2024 Code/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Code/src/main/java/frc/robot/RobotContainer.java
@@ -70,7 +70,7 @@ public class RobotContainer {
     SubsystemContainer.swerveSubsystem.setDefaultCommand(new SwerveDefaultCommand(m_driverController));
     SubsystemContainer.intakeSubsystem.setDefaultCommand(m_intakeDefaultCommand);
     SubsystemContainer.climbSubsystem.setDefaultCommand(m_climbDefaultCommand);
-    SubsystemContainer.armSubsystem.setDefaultCommand(new ArmAngleCommand(-25));
+    SubsystemContainer.armSubsystem.setDefaultCommand(new ArmAngleCommand(0.0));
     SubsystemContainer.shooterSubsystem.setDefaultCommand(new ShooterDefaultCommand());
     SubsystemContainer.limelightSubsystem.setDefaultCommand(m_LimelightDefaultCommand);
 
--- 2024 Code/src/main/java/frc/robot/commands/ArmAngleCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/ArmAngleCommand.java
@@ -27,7 +27,7 @@ public class ArmAngleCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_armSubsystem.setArmReferenceAngle(m_angle);
+    m_armSubsystem.setArmReferenceAngle(PIDChangerSimulationArm.GetTargetAngle());
   }
 
   // Called once the command ends or is interrupted.
