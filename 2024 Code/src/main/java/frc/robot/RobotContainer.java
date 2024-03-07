aec0886c750f5fb39084fe109801f214587ef017
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-03-07 07:34:18 -0600

    Update RobotContainer.java

--- 2024 Code/src/main/java/frc/robot/RobotContainer.java
+++ 2024 Code/src/main/java/frc/robot/RobotContainer.java
@@ -87,9 +87,8 @@ public class RobotContainer {
     m_driverController.leftStick()
         .whileFalse(new InstantCommand(() -> SubsystemContainer.swerveSubsystem.setFastMode(false)));
 
-    MagicShootCommand m_magicAmpShootCommand = new MagicShootCommand();
     m_driverController.a().onTrue(new SequentialCommandGroup(SubsystemContainer.swerveSubsystem.m_pathfindAmp,
-        new MagicAmpChargeCommand(m_buttonsController), m_magicAmpShootCommand));
+        new MagicAmpChargeCommand(m_buttonsController), new MagicShootCommand()));
 
     m_driverController.b().onTrue(new InstantCommand(() -> {
       for (int i = 0; i < 4; i++) {
@@ -99,12 +98,12 @@ public class RobotContainer {
 
     // shooter buttons
     m_buttonsController.a().whileTrue(new SequentialCommandGroup(
-        new MagicAmpChargeCommand(m_buttonsController), m_magicAmpShootCommand));
+        new MagicAmpChargeCommand(m_buttonsController), new MagicShootCommand()));
 
     // separate button binding to left bumper contained within the magic speaker
     // charge command
     m_buttonsController.y().whileTrue(new SequentialCommandGroup(
-        new MagicSpeakerChargeCommand(m_angleCalculate, m_buttonsController), m_magicAmpShootCommand));
+        new MagicSpeakerChargeCommand(m_angleCalculate, m_buttonsController), new MagicShootCommand()));
 
     m_buttonsController.x().whileTrue(
         new ShootManualCommand(Constants.ArmConstants.SUBWOOFER_ANGLE, Constants.ShooterSpeeds.SHOOTING_SPEED));
