50f710de6e8803c6a20f753555db845b38534876
andrew gurik <gurellia53@gmail.com>
2024-03-12 00:39:32 -0500

    test that the interpolated angle increases as the robot moves away from the target
    cleanup

--- 2024 Code/src/test/java/CalculateAngleTest.java
+++ 2024 Code/src/test/java/CalculateAngleTest.java
@@ -1,16 +1,23 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.wpi.first.hal.HAL;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
 import frc.robot.util.CalculateAngle;
 
 public class CalculateAngleTest {
 
     static final double DELTA = 1e-4;
+    private static final DecimalFormat two_points = new DecimalFormat("0.00");
+
+    // the thing under test
     CalculateAngle m_calculate_angle;
 
     @BeforeEach
@@ -23,7 +30,52 @@ public class CalculateAngleTest {
     }
 
     @Test
-    void defaultInterpolation() {
+    void interpolateZeroPoint() {
         assertEquals(1.22654, m_calculate_angle.InterpolateAngle(new Point2D.Double(0.0, 0.0)), DELTA);
+    }
+
+    @Test
+    void interpolatePointClosestToSpeaker() {
+        assertEquals(1.22654, m_calculate_angle.InterpolateAngle(new Point2D.Double(1.625, 0.0)), DELTA);
+    }
+
+    public class PointAnglePair {
+        private Point2D pt;
+        private double angle;
+
+        PointAnglePair(double x, double y, CalculateAngle calc) {
+            pt = new Point2D.Double(x, y);
+            angle = calc.InterpolateAngle(pt);
+        }
+
+        public double Angle() {
+            return angle;
+        }
+
+        public String toString() {
+            return "x: " + two_points.format(pt.getX()) + "  y: " + two_points.format(pt.getY()) + "  angle: "
+                    + two_points.format(angle);
+        }
+    };
+
+    @Test
+    void interpolateAlongXAndVerifyAngleIncreases() {
+        // populate points and calculate angles
+        ArrayList<PointAnglePair> points = new ArrayList<PointAnglePair>();
+        for (double x = 1.6; x < 4.0; x = x + 0.05) {
+            points.add(new PointAnglePair(x, 0.0, m_calculate_angle));
+        }
+
+        // verify that angle increases
+        for (int i = 0; i < points.size() - 1; i++) {
+            PointAnglePair p_small = points.get(i);
+            PointAnglePair p_big = points.get(i + 1);
+
+            String SUFFIX = p_small.Angle() > p_big.Angle() ? " x" : "";
+            System.out.println(p_small + SUFFIX);
+
+            assertTrue(p_big.Angle() > p_small.Angle(),
+                    "big point:" + p_big.Angle() + " small point:" + p_small.Angle());
+        }
     }
 }
