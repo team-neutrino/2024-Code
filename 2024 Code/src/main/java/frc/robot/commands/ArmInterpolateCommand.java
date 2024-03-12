0bf65e023140336e46ca0ef487bb4cb5c1e57d05
andrew gurik <gurellia53@gmail.com>
2024-03-11 22:56:34 -0500

    update usages of CalculateAngle to inject the robot point

--- 2024 Code/src/main/java/frc/robot/commands/ArmInterpolateCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/ArmInterpolateCommand.java
@@ -7,15 +7,18 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.SubsystemContainer;
 import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.util.CalculateAngle;
 
 public class ArmInterpolateCommand extends Command {
   private ArmSubsystem m_armSubsystem;
+  private SwerveSubsystem m_swerve;
   private CalculateAngle m_angleCalculate;
 
   public ArmInterpolateCommand(CalculateAngle p_angleCalculate) {
     m_angleCalculate = p_angleCalculate;
     m_armSubsystem = SubsystemContainer.armSubsystem;
+    m_swerve = SubsystemContainer.swerveSubsystem;
     addRequirements(m_armSubsystem);
   }
 
@@ -27,7 +30,7 @@ public class ArmInterpolateCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_armSubsystem.setArmReferenceAngle(m_angleCalculate.InterpolateAngle());
+    m_armSubsystem.setArmReferenceAngle(m_angleCalculate.InterpolateAngle(m_swerve.RobotPoint()));
   }
 
   // Called once the command ends or is interrupted.
