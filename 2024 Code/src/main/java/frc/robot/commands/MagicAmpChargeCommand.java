0a769b3f4648a45eb5600c72d4418fc7ac519116
Allen Cai <allencaicm@gmail.com>
2024-03-12 20:28:39 -0500

    does he know?

--- 2024 Code/src/main/java/frc/robot/commands/MagicAmpChargeCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/MagicAmpChargeCommand.java
@@ -37,12 +37,7 @@ public class MagicAmpChargeCommand extends Command {
   public void execute() {
     m_armSubsystem.setArmReferenceAngle(Constants.ArmConstants.AMP_POSE);
     m_shooterSubsystem.setTargetRPM(1000);
-
-    if (!m_intakeSubsystem.m_centered) {
-      m_intakeSubsystem.runIndexFeed();
-    } else {
-      m_intakeSubsystem.stopIndex();
-    }
+    m_intakeSubsystem.runIndexFeed();
   }
 
   // Called once the command ends or is interrupted.
