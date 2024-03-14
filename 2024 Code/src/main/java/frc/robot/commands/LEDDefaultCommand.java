1de918971cdf7df6caf7b9669f606bb6a1a3486c
Vienna Rossmanith <sarahames2008@gmail.com>
2024-03-14 18:19:18 -0500

    done

--- 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
@@ -1,10 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.util.SubsystemContainer;
 import frc.robot.Constants.LEDConstants.States;
@@ -14,15 +12,11 @@ public class LEDDefaultCommand extends Command {
   private SwerveSubsystem m_swerveSubsystem;
   private LEDSubsystem m_LEDSubsystem;
   private IntakeSubsystem m_IntakeSubsystem;
-  private ShooterSubsystem m_ShooterSubsystem;
-  private ArmSubsystem m_ArmSubsystem;
 
   public LEDDefaultCommand() {
     m_swerveSubsystem = SubsystemContainer.swerveSubsystem;
     m_LEDSubsystem = SubsystemContainer.LEDSubsystem;
     m_IntakeSubsystem = SubsystemContainer.intakeSubsystem;
-    m_ShooterSubsystem = SubsystemContainer.shooterSubsystem;
-    m_ArmSubsystem = SubsystemContainer.armSubsystem;
     addRequirements(m_LEDSubsystem);
   }
 
@@ -36,10 +30,8 @@ public class LEDDefaultCommand extends Command {
     if (m_swerveSubsystem == null) {
       return;
     }
-    if (m_ShooterSubsystem.approveShoot() && m_ArmSubsystem.getInPosition() && m_IntakeSubsystem.isCentered()) {
+    if (m_IntakeSubsystem.isCentered()) {
       m_LEDSubsystem.setToGreen();
-    } else if (m_IntakeSubsystem.isCentered()) {
-      m_LEDSubsystem.setToCyan();
     } else if (m_swerveSubsystem
         .getCommandState() == States.PATHFINDING) {
       m_LEDSubsystem.setToYellow();
