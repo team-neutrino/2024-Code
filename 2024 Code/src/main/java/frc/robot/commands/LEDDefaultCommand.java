13286c88191ab2b17474a981d73b3cf693599810
Vienna Rossmanith <sarahames2008@gmail.com>
2024-03-14 18:04:37 -0500

    More States

--- 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -14,12 +15,14 @@ public class LEDDefaultCommand extends Command {
   private LEDSubsystem m_LEDSubsystem;
   private IntakeSubsystem m_IntakeSubsystem;
   private ShooterSubsystem m_ShooterSubsystem;
+  private ArmSubsystem m_ArmSubsystem;
 
   public LEDDefaultCommand() {
     m_swerveSubsystem = SubsystemContainer.swerveSubsystem;
     m_LEDSubsystem = SubsystemContainer.LEDSubsystem;
     m_IntakeSubsystem = SubsystemContainer.intakeSubsystem;
     m_ShooterSubsystem = SubsystemContainer.shooterSubsystem;
+    m_ArmSubsystem = SubsystemContainer.armSubsystem;
     addRequirements(m_LEDSubsystem);
   }
 
@@ -34,15 +37,15 @@ public class LEDDefaultCommand extends Command {
       return;
     }
     if (m_IntakeSubsystem.isCentered()) {
-      m_LEDSubsystem.setToGreen();
+      m_LEDSubsystem.setToCyan();
     } else if (m_swerveSubsystem
         .getCommandState() == States.PATHFINDING) {
       m_LEDSubsystem.setToYellow();
     } else if (m_swerveSubsystem
         .getCommandState() == (States.AUTOALIGN)) {
       m_LEDSubsystem.setToBlue();
-    } else if (m_ShooterSubsystem.approveShoot()) {
-      m_LEDSubsystem.setToCyan();
+    } else if (m_ShooterSubsystem.approveShoot() && m_ArmSubsystem.getInPosition() && m_IntakeSubsystem.isCentered()) {
+      m_LEDSubsystem.setToGreen();
     } else if (m_IntakeSubsystem.tooFarNote()) {
       m_LEDSubsystem.setToPurple();
     } else {
