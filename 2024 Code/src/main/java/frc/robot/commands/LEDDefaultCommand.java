fb510a49319b469b93e3ba282d12ec55c4efcd14
Vienna Rossmanith <sarahames2008@gmail.com>
2024-03-14 17:53:52 -0500

    More States

--- 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.util.SubsystemContainer;
 import frc.robot.Constants.LEDConstants.States;
@@ -12,11 +13,13 @@ public class LEDDefaultCommand extends Command {
   private SwerveSubsystem m_swerveSubsystem;
   private LEDSubsystem m_LEDSubsystem;
   private IntakeSubsystem m_IntakeSubsystem;
+  private ShooterSubsystem m_ShooterSubsystem;
 
   public LEDDefaultCommand() {
     m_swerveSubsystem = SubsystemContainer.swerveSubsystem;
     m_LEDSubsystem = SubsystemContainer.LEDSubsystem;
     m_IntakeSubsystem = SubsystemContainer.intakeSubsystem;
+    m_ShooterSubsystem = SubsystemContainer.shooterSubsystem;
     addRequirements(m_LEDSubsystem);
   }
 
@@ -38,6 +41,8 @@ public class LEDDefaultCommand extends Command {
     } else if (m_swerveSubsystem
         .getCommandState() == (States.AUTOALIGN)) {
       m_LEDSubsystem.setToBlue();
+    } else if (m_ShooterSubsystem.approveShoot()) {
+      m_LEDSubsystem.setToCyan();
     } else if (m_IntakeSubsystem.tooFarNote()) {
       m_LEDSubsystem.setToPurple();
     } else {
