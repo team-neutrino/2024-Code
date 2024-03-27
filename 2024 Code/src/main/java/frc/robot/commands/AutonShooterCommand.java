dddb7380d2a57702b860d40c555d766cc8f2244f
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-03-27 17:53:43 -0500

    made intake and shooting each one command

--- 2024 Code/src/main/java/frc/robot/commands/AutonShooterCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/AutonShooterCommand.java
@@ -5,18 +5,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.util.CalculateAngle;
 import frc.robot.util.SubsystemContainer;
 
 public class AutonShooterCommand extends Command {
 
   private ShooterSubsystem m_shooterSubsystem;
   private double m_rpm;
+  private ArmSubsystem m_armSubsystem;
+  private SwerveSubsystem m_swerve;
+  private CalculateAngle m_angleCalculate;
+  private IntakeSubsystem m_intakeSubsystem;
 
-  public AutonShooterCommand(double p_rpm) {
+  public AutonShooterCommand(double p_rpm, CalculateAngle p_angleCalculate) {
     m_shooterSubsystem = SubsystemContainer.shooterSubsystem;
     m_rpm = p_rpm;
-    addRequirements(m_shooterSubsystem);
+    m_angleCalculate = p_angleCalculate;
+    m_armSubsystem = SubsystemContainer.armSubsystem;
+    m_swerve = SubsystemContainer.swerveSubsystem;
+    m_intakeSubsystem = SubsystemContainer.intakeSubsystem;
+    addRequirements(m_shooterSubsystem, m_armSubsystem, m_intakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -28,6 +40,12 @@ public class AutonShooterCommand extends Command {
   @Override
   public void execute() {
     m_shooterSubsystem.setTargetRPM(m_rpm);
+    m_armSubsystem.setArmReferenceAngle(m_angleCalculate.InterpolateAngle(m_swerve.GetSpeakerToRobot()));
+    if (m_armSubsystem.getInPosition() && m_shooterSubsystem.approveShoot()) {
+      m_intakeSubsystem.runIndexShoot();
+    } else {
+      m_intakeSubsystem.stopIndex();
+    }
   }
 
   // Called once the command ends or is interrupted.
