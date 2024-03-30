a16e35a7c0ace0274774afe77eef51db3a646a82
Allen Cai <allencaicm@gmail.com>
2024-03-30 11:05:59 -0500

    led cleanup

--- 2024 Code/src/main/java/frc/robot/commands/AutoAlignCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/AutoAlignCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import frc.robot.util.SubsystemContainer;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
 
