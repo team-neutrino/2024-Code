46aa2e92f3ac5bfc38b9cb1cc24d3c43475c14b4
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-04-01 18:26:04 -0500

    >:D

--- 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/LEDDefaultCommand.java
@@ -1,9 +1,12 @@
 package frc.robot.commands;
 
+import frc.robot.Constants.SwerveConstants;
+import frc.robot.Constants.AprilTagConstants.RED_ALLIANCE_IDS;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.util.SubsystemContainer;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -13,12 +16,14 @@ public class LEDDefaultCommand extends Command {
   private ShooterSubsystem m_shooterSubsystem;
   private ArmSubsystem m_armSubsystem;
   private IntakeSubsystem m_intakeSubsystem;
+  private SwerveSubsystem m_swerveSusbsystem;
 
   public LEDDefaultCommand() {
     m_LEDSubsystem = SubsystemContainer.LEDSubsystem;
     m_shooterSubsystem = SubsystemContainer.shooterSubsystem;
     m_armSubsystem = SubsystemContainer.armSubsystem;
     m_intakeSubsystem = SubsystemContainer.intakeSubsystem;
+    m_swerveSusbsystem = SubsystemContainer.swerveSubsystem;
 
     addRequirements(m_LEDSubsystem);
   }
@@ -30,7 +35,9 @@ public class LEDDefaultCommand extends Command {
 
   @Override
   public void execute() {
-    if (m_shooterSubsystem.approveShoot() && m_armSubsystem.getInPosition() && m_intakeSubsystem.isNoteReady()) {
+    if (AutoAlignedToAmp()) {
+      m_LEDSubsystem.setToRed();
+    } else if (m_shooterSubsystem.approveShoot() && m_armSubsystem.getInPosition() && m_intakeSubsystem.isNoteReady()) {
       m_LEDSubsystem.setToGreen();
     } else if (m_intakeSubsystem.isNoteReady()) {
       m_LEDSubsystem.setToCyan();
@@ -40,6 +47,16 @@ public class LEDDefaultCommand extends Command {
       m_LEDSubsystem.setToOrange();
     }
 
+  }
+
+  private boolean AutoAlignedToAmp() {
+    if (SubsystemContainer.alliance.isRedAlliance()) {
+      return Math.abs(m_swerveSusbsystem.getPose().getX() - SwerveConstants.SPEAKER_RED_SIDE.getX()) <= .05
+          && Math.abs(m_swerveSusbsystem.getPose().getY() - SwerveConstants.SPEAKER_RED_SIDE.getY()) <= 2;
+    } else {
+      return Math.abs(m_swerveSusbsystem.getPose().getX() - SwerveConstants.SPEAKER_BLUE_SIDE.getX()) <= .05
+          && Math.abs(m_swerveSusbsystem.getPose().getY() - SwerveConstants.SPEAKER_BLUE_SIDE.getY()) <= 2;
+    }
   }
 
   @Override
