5b125837fff8feb61fbea1bad5119afdd376cf2e
Vienna Rossmanith <sarahames2008@gmail.com>
2024-04-05 19:46:28 -0500

    Half way there

--- 2024 Code/src/main/java/frc/robot/commands/ArmDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/ArmDefaultCommand.java
@@ -31,7 +31,6 @@ public class ArmDefaultCommand extends Command {
   @Override
   public void execute() {
     SubsystemContainer.armSubsystem.setArmReferenceAngle(ArmConstants.INTAKE_POSE);
-    m_armSubsystem.setCommandState(States.CLIMBING);
   }
 
   // Called once the command ends or is interrupted.
