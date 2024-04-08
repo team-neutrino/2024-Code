9d019174d75328b7204bd6d0b779f2611cd99fdb
Neutrino Controls <152452752+allencai11@users.noreply.github.com>
2024-04-08 17:35:01 -0500

    Update IntakeDefaultCommand.java

--- 2024 Code/src/main/java/frc/robot/commands/IntakeDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/IntakeDefaultCommand.java
@@ -1,32 +1,43 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.util.SubsystemContainer;
 
 public class IntakeDefaultCommand extends Command {
+    Timer timer;
     IntakeSubsystem m_intakeSubsystem;
 
     public IntakeDefaultCommand() {
         m_intakeSubsystem = SubsystemContainer.intakeSubsystem;
+        timer = new Timer();
         addRequirements(m_intakeSubsystem);
     }
 
     @Override
     public void initialize() {
+        timer.start();
     }
 
     @Override
     public void execute() {
-        if (m_intakeSubsystem.hasNote() && !m_intakeSubsystem.isNoteReady()) {
-            m_intakeSubsystem.smartIntake();
+        if (!timer.hasElapsed(2)) {
+            m_intakeSubsystem.runIndexJitter();
         } else {
-            m_intakeSubsystem.defaultIntake();
+            timer.stop();
+
+            if (m_intakeSubsystem.hasNote() && !m_intakeSubsystem.isNoteReady()) {
+                m_intakeSubsystem.smartIntake();
+            } else {
+                m_intakeSubsystem.defaultIntake();
+            }
         }
     }
 
     @Override
     public void end(boolean interrupted) {
+        timer.reset();
     }
 
     @Override
