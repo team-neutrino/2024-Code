5454424d10b66473907128b1a90a1241ad6531bd
Allen Cai <allencaicm@gmail.com>
2024-04-17 15:35:08 -0500

    old data not from ice

--- 2024 Code/src/main/java/frc/robot/util/CalculateAngle.java
+++ 2024 Code/src/main/java/frc/robot/util/CalculateAngle.java
@@ -38,20 +38,20 @@ public class CalculateAngle {
 
     public CalculateAngle() {
 
-        bilinearMap.put(new PolarCoord(2.15, 0), 5.0);
-        bilinearMap.put(new PolarCoord(3.1, 0), 11.0);
-        bilinearMap.put(new PolarCoord(4.0, 0.0), 15.0);
-        bilinearMap.put(new PolarCoord(4.9, 0.0), 16.0);
+        bilinearMap.put(new PolarCoord(2.15, 0), 5.58407);
+        bilinearMap.put(new PolarCoord(3.1, 0), 11.75);
+        bilinearMap.put(new PolarCoord(4.0, 0.0), 15.68);
+        bilinearMap.put(new PolarCoord(4.9, 0.0), 16.72);
 
-        bilinearMap.put(new PolarCoord(2.15, pi / 8), 6.5);
-        bilinearMap.put(new PolarCoord(3.1, pi / 8), 9.0);
-        bilinearMap.put(new PolarCoord(4.0, pi / 8), 14.0);
-        bilinearMap.put(new PolarCoord(4.9, pi / 8), 16.5);
+        bilinearMap.put(new PolarCoord(2.15, pi / 8), 7.26359);
+        bilinearMap.put(new PolarCoord(3.1, pi / 8), 9.5026);
+        bilinearMap.put(new PolarCoord(4.0, pi / 8), 14.7259);
+        bilinearMap.put(new PolarCoord(4.9, pi / 8), 17.28);
 
-        bilinearMap.put(new PolarCoord(2.15, pi / 4), 7.5);
-        bilinearMap.put(new PolarCoord(3.1, pi / 4), 12.0);
-        bilinearMap.put(new PolarCoord(4.0, pi / 4), 16.0);
-        bilinearMap.put(new PolarCoord(4.9, pi / 4), 17.5);
+        bilinearMap.put(new PolarCoord(2.15, pi / 4), 8.03);
+        bilinearMap.put(new PolarCoord(3.1, pi / 4), 12.7926);
+        bilinearMap.put(new PolarCoord(4.0, pi / 4), 16.6866);
+        bilinearMap.put(new PolarCoord(4.9, pi / 4), 18.133);
 
         for (PolarCoord cur_key : bilinearMap.keySet()) {
             if (!col.contains(cur_key.x)) {
