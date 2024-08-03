556e9f5d67e21a17400deeaea92896464f83a6fc
Vienna Rossmanith <sarahames2008@gmail.com>
2024-08-02 19:28:08 -0500

    ss

--- 2024 Code/src/main/java/frc/robot/commands/PhotonVisionDefaultCommand.java
+++ 2024 Code/src/main/java/frc/robot/commands/PhotonVisionDefaultCommand.java
@@ -1,26 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class PhotonVisionDefaultCommand extends Command {
+  /** Creates a new PhotonVisionDefaultCommand. */
+  public PhotonVisionDefaultCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
 
-    public PhotonVisionDefaultCommand() {
-    }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
 
-    @Override
-    public void initialize() {
-    }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
 
-    @Override
-    public void execute() {
-    }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
-    @Override
-    public void end(boolean interrupted) {
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
