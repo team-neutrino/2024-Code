be5e49956b55dbf278dd2e7421b5cf69f010eb84
Allen <152452752+allencai11@users.noreply.github.com>
2024-10-14 18:38:10 -0500

    Move code from old file to new file

--- 2024 Code/src/main/java/frc/robot/subsystems/wrapper/SwerveSim.java
+++ 2024 Code/src/main/java/frc/robot/subsystems/wrapper/SwerveSim.java
@@ -6,10 +6,29 @@ package frc.robot.subsystems.wrapper;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+
 public class SwerveSim extends SubsystemBase {
-  /** Creates a new SweveSim. */
+  NetworkTableInstance nt = NetworkTableInstance.getDefault();
+  DoubleTopic Motor_Voltage = nt.getDoubleTopic("swerve/motor_voltage");
+  final DoublePublisher motorVoltage_pub;
+
+  /** Creates a new SwerveSim. */
   public SwerveSim() {
+    motorVoltage_pub = Motor_Voltage.publish();
+    motorVoltage_pub.setDefault(0.0);
   }
+
+  private TalonFX m_talonFX = new TalonFX(4);
+
+  TalonFXSimState m_talonFXSim = m_talonFX.getSimState();
+  double fakeMotorVoltage = m_talonFXSim.getMotorVoltage();
 
   @Override
   public void periodic() {
