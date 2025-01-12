5374c21c08b8ecebed6e6c795178a0f45527fd35
Matt Reed <enfcyco@hotmail.com>
2025-01-12 04:57:24 -0600

    Update RobotTest.java

--- 2024 Code/src/test/java/frc/robot/RobotTest.java
+++ 2024 Code/src/test/java/frc/robot/RobotTest.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import org.junit.jupiter.api.Test;
+import frc.robot.subsystems.ArmSubsystem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,6 +9,7 @@ public class RobotTest {
 
     @Test
     void testSomething() {
-        assertEquals(10, 5+4);
+        assertEquals(10, 5+5);
+        new ArmSubsystem();
     }
 }
