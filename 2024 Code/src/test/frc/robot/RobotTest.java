40df62eaa8203c2065b99f06bf4798e1adc57851
Matt Reed <enfcyco@hotmail.com>
2025-01-11 20:31:03 -0600

    Create first test

---
+++ 2024 Code/src/test/frc/robot/RobotTest.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RobotTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(10, 5+5)
+    }
+}
