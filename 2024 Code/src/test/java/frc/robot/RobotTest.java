2b48fd17fe46e5d90c69205b4731b0f50a649025
Matt Reed <enfcyco@hotmail.com>
2025-01-12 05:15:38 -0600

    Update RobotTest.java

--- 2024 Code/src/test/java/frc/robot/RobotTest.java
+++ 2024 Code/src/test/java/frc/robot/RobotTest.java
@@ -11,7 +11,7 @@ public class RobotTest {
     void testSomething() {
         assertEquals(10, 5+5);
         var armSubsysem = new ArmSubsystem();
-        var armDeafault = armSubsysem.getArmAngleDegrees();
+        var armDefault = armSubsysem.getArmAngleDegrees();
         System.out.println("armDefault " + armDefault);
         asserTrue(1.92 < armDefault);
         asserTrue(armDefault < 1.93);
