3fe2a3d24e2914ae4ccd380072bf3ccb9d4fc083
Matt Reed <enfcyco@hotmail.com>
2025-01-12 07:40:41 -0600

    Update RobotTest.java

--- 2024 Code/src/test/java/frc/robot/RobotTest.java
+++ 2024 Code/src/test/java/frc/robot/RobotTest.java
@@ -9,20 +9,38 @@ public class RobotTest {
 
     @Test
     void testTargetAngle() {
-        var armSubsysem = new ArmSubsystem();
-        var armDefault = armSubsysem.getArmAngleDegrees();
+        var armSubsystem = new ArmSubsystem();
+        var armDefault = armSubsystem.getArmAngleDegrees();
         System.out.println("armDefault " + armDefault);
         //this looks to be random at the moment
         // assertTrue(1.92 < armDefault);
         // assertTrue(armDefault < 1.93);
 
-        var defaultTargetAngle = armSubsysem.getTargetAngle();
+        var defaultTargetAngle = armSubsystem.getTargetAngle();
         System.out.println("targetAngle " + defaultTargetAngle);
         assertEquals(-27.0, defaultTargetAngle);
 
-        armSubsysem.setClimbReferenceAngle();
-        var newTargetAngle = armSubsysem.getTargetAngle();
+        armSubsystem.setClimbReferenceAngle();
+        var newTargetAngle = armSubsystem.getTargetAngle();
         System.out.println("newTargetAngle " + newTargetAngle);
         assertEquals(-20.0, newTargetAngle);
     }
-}
+
+    @Test
+    void testDefaultArmAngle() {
+        var armSubsystem = new ArmSubsystem();
+
+        var defaultTargetAngle = armSubsystem.getTargetAngle();
+        assertEquals(-27.0, defaultTargetAngle);
+    }
+
+    @Test
+    void testClimbAngle() {
+        double expectedClimbAngle = -20.0;
+        var armSubsystem = new ArmSubsystem();
+
+        armSubsystem.setClimbReferenceAngle();
+
+        var newTargetAngle = armSubsystem.getTargetAngle();
+        assertEquals(expectedClimbAngle, newTargetAngle);
+    }}
